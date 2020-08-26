4045abbf1c175dddecbabd8e7aaee48b7b8354c0
Chris Wheat <chriswheat@zoologi195.zoologi.su.se>
2020-08-26 14:57:58 +0200

    udpdate

---
+++ data/illumina_data.sh
@@ -0,0 +1,10 @@
+
+# location of reads on MILES
+/mnt/griffin/Reads/Pieris_macdounghii_data
+
+# single individual Illumina sequence data
+Pieris_macdounghii.USA.14.Colorado.Pmac000.Thorax.1_1.fq.gz
+Pieris_macdounghii.USA.14.Colorado.Pmac000.Thorax.1_2.fq.gz
+
+
+# poolseq data
