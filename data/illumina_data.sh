02295b3830d672b9c18830de22a87a3ddcc2892c
Chris Wheat <chriswheat@zoologi195.zoologi.su.se>
2020-08-26 15:40:09 +0200

    update

--- data/illumina_data.sh
+++ data/illumina_data.sh
@@ -8,3 +8,8 @@ Pieris_macdounghii.USA.14.Colorado.Pmac000.Thorax.1_2.fq.gz
 
 
 # poolseq data
+# from local
+
+scp -r P4308 chrwhe@miles.zoologi.su.se:/mnt/griffin/Reads/Pieris_macdounghii_data
+
+/mnt/griffin/Reads/Pieris_macdounghii_data
