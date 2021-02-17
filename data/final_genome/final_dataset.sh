43f0190788eea5925b4dda80800d7c764843c2d2
rstewa03 <63721115+rstewa03@users.noreply.github.com>
2021-02-17 11:00:15 +0100

    Update final_dataset.sh

--- data/final_genome/final_dataset.sh
+++ data/final_genome/final_dataset.sh
@@ -1,4 +1,4 @@
 
 
 # genome
-scp chrwhe@miles.zoologi.su.se:/mnt/griffin/racste/P_macdunnoughii/Pmac_genome_versions/faDnaPolishing/Pmac000.1_polished.cleaned.fasta.gz .
+scp chrwhe@miles.zoologi.su.se:/mnt/griffin/racste/P_macdunnoughii/Pmac_genome_versions/faDnaPolishing/PmacMums_polished.cleaned.fasta.gz .
